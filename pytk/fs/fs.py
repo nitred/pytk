74eff1ecee5496b85af7e558fccd1cfc61f7893c
Nitish Reddy Koripalli <nitish.k.reddy@recogizer.de>
2016-10-21 19:06:56 +0200

    refactor

--- pytk/fs/fs.py
+++ pytk/fs/fs.py
@@ -3,7 +3,7 @@
 import os
 
 
-def get_list_of_files_from_dir(path):
+def get_list_of_file_names_from_dir(path):
     """A function that returns a list of files in a given path."""
     try:
         return [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
@@ -12,14 +12,14 @@ def get_list_of_files_from_dir(path):
         raise
 
 
-def get_list_of_files_from_dir_ascending(path):
+def get_list_of_file_names_from_dir_ascending(path):
     """A function that returns a list of files in a given path in ascending order."""
-    return sorted(get_list_of_files_from_dir(path))
+    return sorted(get_list_of_file_names_from_dir(path))
 
 
 def get_list_of_file_paths_from_dir(path):
     """A function that returns a list of files with full filenames in a given path."""
-    return [os.path.join(path, f) for f in get_list_of_files_from_dir(path)]
+    return [os.path.join(path, f) for f in get_list_of_file_names_from_dir(path)]
 
 
 def get_list_of_file_paths_from_dir_ascending(path):
