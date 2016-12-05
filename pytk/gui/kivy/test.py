cad86d554c65e2f1b2e7953a8c08c82cda261011
Nitish Reddy Koripalli <nitish.k.reddy@gmail.com>
2016-12-05 05:10:44 +0100

    added gui

---
+++ pytk/gui/kivy/test.py
@@ -0,0 +1,17 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Docstring for module."""
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class MyApp(App):
+    """."""
+
+    def build(self):
+        """."""
+        return Label(text='Hello World')
+
+
+if __name__ == '__main__':
+    MyApp().run()
